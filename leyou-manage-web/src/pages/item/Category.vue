57101650da22dbfeefcc2b36fd607eb2c9049f10
hjb <645246861@qq.com>
2020-05-09 13:00:29 +0800

    乐游商城前台

---
+++ leyou-manage-web/src/pages/item/Category.vue
@@ -0,0 +1,54 @@
+<template>
+  <v-card>
+      <v-flex xs12 sm10>
+        <v-tree url="/item/category/list"
+                :isEdit="isEdit"
+                @handleAdd="handleAdd"
+                @handleEdit="handleEdit"
+                @handleDelete="handleDelete"
+                @handleClick="handleClick"
+        />
+      </v-flex>
+  </v-card>
+</template>
+
+<script>
+  export default {
+    name: "category",
+    data() {
+      return {
+        isEdit:true
+      }
+    },
+    methods: {
+      handleAdd(node) {
+        console.log("add .... ");
+        console.log(node);
+        this.$http.get("/item/category/add",{
+          params:{
+            id:node.id,
+            name:node.name,
+            parentId:node.parentId,
+            isParent:node.isParent,
+            sort:node.sort
+          }
+        }).then(resp => { // 这里使用箭头函数
+          console.log(resp)
+        })
+      },
+      handleEdit(id, name) {
+        console.log("edit... id: " + id + ", name: " + name)
+      },
+      handleDelete(id) {
+        console.log("delete ... " + id)
+      },
+      handleClick(node) {
+        console.log(node)
+      }
+    }
+  };
+</script>
+
+<style scoped>
+
+</style>
